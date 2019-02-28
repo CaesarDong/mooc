cbc55e695b766851fe21c076a2692a7713369e05
CaesarDong <1686150650@qq.com>
2019-02-28 11:29:39 +0800

    28

--- testData.java
+++ testData.java
@@ -3,3 +3,8 @@ aaaaaaaaaaaaaaaaa
 bbbbbbbbbbbbbbbbbb
 
 ccccccccccccccccc
+
+eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxMTAwNjk4Mjc2MTgyNzU3Mzc2IiwiaWF0IjoxNTUxMjYxOTY5LCJyb2xlcyI6InVzZXIiLCJleHAiOjE1NTEyOTc5Njl9.YGXzXWjlXKe9N6olUV9jTrDGVbxdcDIrPEDxDoBJYUs
+
+
+select friend0_.userid as userid1_0_, friend0_.friendid as friendid2_0_, friend0_.islike as islike3_0_ from tb_friend friend0_ where friend0_.userid=? and friend0_.friendid=?
